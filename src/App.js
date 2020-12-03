f1561cb758bd1b50319281097a8d6eeed2d04c79
Liydmyla Chubko <luda.chubko@gmail.com>
2020-12-03 21:59:03 +0200

    practise2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import UsersComponent from "./components/users/Users Component";
 import CountriesComponent from "./components/countries/Countries Component";
 import PetsComponent from "./components/pets/Pets Component";
 import AllUsersComponent from "./components/All Users Component/All Users Component";
+import AllPeople from "./components/people/All people";
 
 class App extends Component {
 
     render() {
         return(
             <div>
-                <CarsComponent/>
-                <CitiesComponent/>
-                <UsersComponent/>
-                <CountriesComponent/>
-                <PetsComponent/>
-                <AllUsersComponent/>
+                {/*<CarsComponent/>*/}
+                {/*<CitiesComponent/>*/}
+                {/*<UsersComponent/>*/}
+                {/*<CountriesComponent/>*/}
+                {/*<PetsComponent/>*/}
+                {/*<AllUsersComponent/>*/}
+                <AllPeople/>
             </div>
     )
     }
