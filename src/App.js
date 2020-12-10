56851de256ff0785c0568477736b3482c017ef34
Liydmyla Chubko <luda.chubko@gmail.com>
2020-12-10 12:44:23 +0200

    homework4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, {Component} from 'react';
-import CitiesComponent from "./components/cities/Cities Component";
-import CarsComponent from "./components/cars/Cars Component";
-import UsersComponent from "./components/users/Users Component";
-import CountriesComponent from "./components/countries/Countries Component";
-import PetsComponent from "./components/pets/Pets Component";
+import AllUsers from "./component/users/AllUsers";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import AllPosts from "./component/posts/AllPosts";
+import AllComments from "./component/comments/AllComments";
+import AllUserPosts from "./component/posts/AllUserPosts";
+
 
 class App extends Component {
 
     render() {
         return(
-            <div>
-                <CarsComponent/>
-                <CitiesComponent/>
-                <UsersComponent/>
-                <CountriesComponent/>
-                <PetsComponent/>
-            </div>
+                <Router>
+                    <div>
+                        <div>
+                            <Link to={'/users'}>
+                                users
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to={'/posts'}>
+                            posts
+                        </Link>
+                        </div>
+                        <div>
+                            <Link to={'/comments'}>
+                                comments
+                            </Link>
+                        </div>
+
+                        <Switch>
+                            <Route path={'/users'} render={()=>{return <AllUsers/>}}/>
+                            <Route path={'/posts'} render={()=>{return <AllUserPosts/>}}/>
+                            <Route path={'/comments'} render={()=>{return <AllComments/>}}/>
+                        </Switch>
+                    </div>
+                </Router>
     )
     }
 }
