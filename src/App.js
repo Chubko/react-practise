fcda9375cc12735ede52489f949540f45126374b
Liydmyla Chubko <luda.chubko@gmail.com>
2021-01-02 20:19:23 +0200

    practise10_redux_form_select

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
-import React, {Component} from 'react';
-import {useSelector, useDispatch} from "react-redux";
-
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 export default function App() {
     let users = useSelector(({users}) => users);
+    let choicesForDelete = useSelector(({choicesForDelete}) => choicesForDelete);
     const dispatch = useDispatch();
 
-    const addUser = (e)=>{
-       e.preventDefault();
-       const userName = e.target[0].value;
-       const userAge= +e.target[1].value;
-       const userId = new Date().getTime();
-       users = [...users, {userName, userAge, userId}]
-       return dispatch({type: 'ADD', payload: users})
-    }
-    console.log(users)
-
-    let selectedId;
-
-    const selectUser = (e) => {
-        selectedId = e.target.value.userId;
-    console.log(selectedId)
-    }
-    console.log(selectedId)
-
-
-    // const selectUser = (event)=>{
-    //     users.map(user => {
-    //         // if(user.options[0].selected == true){
-    //             console.log(user.options)
-    //         // }
-    //     })
-    //
-    // }
-    const removeUser = (e, id)=>{
+    const addUser = (e)=> {
         e.preventDefault();
-        let usersUpdate = users.filter((user)=>user.userId !==id)
-        // console.log(e.target)
-        // const deletedUser = users.find(value => value.id === +e.target[2].value);
-        console.log(e.target)
-        return dispatch({type: 'REMOVE', payload: usersUpdate})
+        const userName = e.target[0].value;
+        const userAge = +e.target[1].value;
+        const userId = Math.floor(Date.now()/1000);
+        users = [...users, {userName, userAge, userId}]
+        return dispatch({type: 'ADD', payload: users})
     }
 
+    const selectUser = (e)=>{
+        e.preventDefault();
+        const id = +e.target.value.split('-')[2];
+        return dispatch({type: 'SELECT', payload: id})
+    }
 
+    const removeUser = (e)=>{
+        e.preventDefault();
+        return dispatch({type: 'REMOVE', payload: choicesForDelete})
+    }
 
     return (
         <div>
@@ -52,17 +34,15 @@ export default function App() {
                 <button>save</button>
             </form>
             <form>
-                <select status ={false} onChange={}>
+                <select onClick={(e)=>selectUser(e)}>
                 {users.map(user =>
                 <option key={user.userId}>
-                    {user.userName}-{user.userAge}
+                    {user.userName}-{user.userAge}-{user.userId}
                 </option>
                 )}
                 </select>
-                <button onClick={removeUser}>remove</button>
+                <button onClick={(e)=>removeUser(e)}>remove</button>
             </form>
-
         </div>
-
     )
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,22 +1,23 @@
 const initialState = {
-    users:[]
-}
+    users:[],
+    choicesForDelete: null
+   }
 
 export const reducer = (state = initialState, action)=>{
     switch (action.type){
         case 'ADD':{
             return {...state, users: action.payload};
         }
+        case 'SELECT':{
+            return {...state, choicesForDelete: action.payload}
+        }
         case 'REMOVE':{
-            // const updatedUsers = state.users.filter(
-            //     (el) => el.id !== action.payload.id
-            // );
-            // return { ...state, users: updatedUsers };
-            return {...state, users: action.payload}
+            const updatedUsers = state.users.filter(
+                (el) => el.userId !== action.payload);
+            return { ...state, users: updatedUsers };
         }
         default:{
             return state;
         }
     }
-
 }
