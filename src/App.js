7bef6845b07716aa12354037a8deec290443cff4
Liydmyla Chubko <luda.chubko@gmail.com>
2021-01-02 13:42:24 +0200

    practise11_redux_form_select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React, {Component} from 'react';
-import CitiesComponent from "./components/cities/Cities Component";
-import CarsComponent from "./components/cars/Cars Component";
-import UsersComponent from "./components/users/Users Component";
-import CountriesComponent from "./components/countries/Countries Component";
-import PetsComponent from "./components/pets/Pets Component";
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import UsersList from "./components/UsersList";
+import Button from "./components/Button";
 
-class App extends Component {
+export default function App(){
+    const users = useSelector(({users}) => users);
+    const dispatch = useDispatch();
 
-    render() {
+    const addUser = (e)=>{
+        e.preventDefault();
+        const userName = e.target[0].value;
+        const userAge= +e.target[1].value;
+        const userId = new Date().getTime();
+        return dispatch({type: 'ADD_USER', payload: {userName, userAge, userId}})
+    }
+
+    const removeUser = (id)=>{
+        return dispatch({type: 'REMOVE_USER', payload: id});
+    }
+
         return(
             <div>
-                <CarsComponent/>
-                <CitiesComponent/>
-                <UsersComponent/>
-                <CountriesComponent/>
-                <PetsComponent/>
+                <form onSubmit={addUser}>
+                    <input placeholder='enter your name'/>
+                    <input placeholder='enter your age'/>
+                    <button>save</button>
+                </form>
+                <hr/>
+                {users.map(user =>
+                    <>
+                        <UsersList
+                            user={user}
+                            key={user.userId}
+                        />
+                        <Button removeUser={removeUser} id={user.userId} value={'delete'}/>
+                        <Button value={'edit'}/>
+                    </>
+                    )}
             </div>
     )
     }
-}
 
-export default App;
+
